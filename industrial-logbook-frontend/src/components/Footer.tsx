d697c9e4586685b0035e0a806ad1ec54796b5b0e
EmmanuelOnjoro <134922677+EmmanuelOnjoro@users.noreply.github.com>
2025-06-08 22:16:36 +0300

    Update Footer.tsx

--- industrial-logbook-frontend/src/components/Footer.tsx
+++ industrial-logbook-frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import "../styles/custom.css";
 const Footer: React.FC = () => (
   <footer className="bg-maroon text-white py-2 px-3 fixed-bottom shadow-sm d-flex justify-content-between align-items-center" style={{ zIndex: 1050 }}>
     <span>&copy; {new Date().getFullYear()} LOGBOOK. All rights reserved.</span>
-    <span className="small">Powered by Your Institution</span>
+    <span className="small">Powered by Kitale nationa Polytechnic </span>
   </footer>
 );
 
